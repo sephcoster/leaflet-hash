7b38d9bcb7ec01f68b39eb3b70c79f5353916939
sephcoster <sephcoster@gmail.com>
2014-11-04 08:57:36 -0500

    Updated to use params object

--- leaflet-hash.js
+++ leaflet-hash.js
@@ -14,14 +14,11 @@
 	};
 
 	L.Hash.parseHash = function(hash) {
-		if(hash.indexOf('#&pos=') === 0) {
-			hash = hash.substr(6);
-		}
-		var args = hash.split("/");
-		if (args.length == 3) {
-			var zoom = parseInt(args[0], 10),
-			lat = parseFloat(args[1]),
-			lon = parseFloat(args[2]);
+		var hashParams = getPageHashParams();
+		if ( typeof hashParams.zoom !='undefined' && typeof hashParams.lat !='undefined' && typeof hashParams.lon !='undefined') {
+			var zoom = parseInt(hashParams.zoom, 10),
+			lat = parseFloat(hashParams.lat),
+			lon = parseFloat(hashParams.lon);
 			if (isNaN(zoom) || isNaN(lat) || isNaN(lon)) {
 				return false;
 			} else {
@@ -38,12 +35,13 @@
 	L.Hash.formatHash = function(map) {
 		var center = map.getCenter(),
 		    zoom = map.getZoom(),
-		    precision = Math.max(0, Math.ceil(Math.log(zoom) / Math.LN2));
+		    precision = Math.max(0, Math.ceil(Math.log(zoom) / Math.LN2)),
+		    params = getPageHashParams();
 
-		return "#&pos=" + [zoom,
-			center.lat.toFixed(precision),
-			center.lng.toFixed(precision)
-		].join("/");
+		addParam('zoom', zoom);
+		addParam('lat', center.lat.toFixed(precision));
+		addParam('lon', center.lng.toFixed(precision));
+		updateUrlHash();
 	},
 
 	L.Hash.prototype = {
@@ -160,3 +158,51 @@
 		this._hash.removeFrom();
 	};
 })(window);
+
+function getPageHashParams(){
+
+    var hashParams = {};
+    var e,
+        a = /\+/g,  // Regex for replacing addition symbol with a space
+        r = /([^!&;=<>]+)(!?[=><]?)([^&;]*)/g,
+        d = function (s) { return decodeURIComponent(s.replace(a, ' ')); },
+        q = window.location.hash.substring(1).replace(/^!\/?/, '');
+
+    while (e = r.exec(q)) {
+      hashParams[d(e[1])] = {
+        values: d(e[3]),
+        comparator: d(e[2])
+      };
+    }
+
+    return hashParams;
+
+  }
+
+function addParam( paramName, values ){
+    var params = getPageHashParams();
+    params[paramName] = {};
+    params[paramName].values = values;
+    params[paramName].comparator = '=';
+    updateUrlHash(params);
+}
+
+// The `generateUrlHash` method builds and returns a URL hash from a set of object parameters
+function updateUrlHash(params) {
+    var newHash,
+    hashParams = [];
+
+    // Loop through params, stringify them and push them into the temp array.
+    function buildHashParam( param, name ) {
+        //console.log('name: ', name);
+        //console.log("param: ", param);
+
+      hashParams.push( name + '=' + param.values );
+
+    }
+
+    _.forEach( params, buildHashParam );
+    //console.log('Hash Params: ', hashParams);
+    newHash = '&' + hashParams.join('&');
+    window.location.hash = newHash;
+}
